c0a8466f48c14f72080ba248a45f2069d28c8bb3
bunkermaster <yann.le-scouarnec@hetic.net>
2018-02-28 16:56:32 +0100

    🐬 Facto

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include "includes/header.php"?>
+    <div class="container theme-showcase" role="main">
+        <div class="jumbotron">
+            <h1>Les Teletubbies</h1>
+            <p>C'est flippant.</p>
+            <span class="label label-danger">Danger</span>
+        </div>
+        <img class="img-thumbnail" alt="Teletubbies" src="img/teletubbies.jpg" data-holder-rendered="true">
+    </div>
+<?php include "includes/footer.php"?>
---
+++ ironmaiden.php
@@ -0,0 +1,10 @@
+<?php include "includes/header.php"?>
+    <div class="container theme-showcase" role="main">
+        <div class="jumbotron">
+            <h1>Iron Maiden!</h1>
+            <p>C'est vieux.</p>
+            <span class="label label-success">SIX SIX SIX!!! the number of the beast!!</span>
+        </div>
+        <img class="img-thumbnail" alt="Iron Maiden" src="img/ironmaiden.jpg" data-holder-rendered="true">
+    </div>
+<?php include "includes/footer.php"?>
