f8e1afdb6a2d78407b75a21ede30e859e703277c
bunkermaster <yann.le-scouarnec@hetic.net>
2018-03-07 17:29:16 +0100

    🤷🏻‍♂️debut de facto de l'organisation des pages'

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 include "includes/header.php";
 require_once "includes/content.php";
-$page = $content['teletubbies'];
+// definition de la page par defaut
+// est-ce que j'ai le parametre p dans l'url? (isset)
+    // si oui, affectation de p dans $currentPage
+    // si non, j'affiche la page par defaut
+// est-ce que ce $currentPage pointe vers une page qui existe?
+    // si oui, j'affiche la page
+    // si non, response code 404 et affichage de la page par defaut
+$page = $content[$_GET['p'] ?? 'teletubbies'];
 ?>
     <div class="container theme-showcase" role="main">
         <div class="jumbotron">
